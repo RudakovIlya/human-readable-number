7af29eca807537fbe8d41004838b713d6b8838b3
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-07-31 16:14:35 +0300

    feat: added solution human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function toReadable(number) {
     if (number === 0) return "zero";
+
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    function convert(num) {
+        if (num < 10) return ones[num];
+        if (num < 20) return teens[num - 10];
+        const ten = Math.floor(num / 10);
+        const unit = num % 10;
+        return tens[ten] + (unit > 0 ? " " + ones[unit] : "");
+    }
+
+    const hundreds = Math.floor(number / 100);
+    const remainder = number % 100;
+
+    let readable = "";
+    if (hundreds > 0) {
+        readable += ones[hundreds] + " hundred";
+        if (remainder > 0) readable += " ";
+    }
+
+    if (remainder > 0) {
+        readable += convert(remainder);
+    }
+
+    return readable;
 };
